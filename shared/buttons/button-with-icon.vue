2f1b9b9c0ab82c39d5b759a81d48365d8f508876
Akhmadov Muhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2024-07-11 20:57:54 +0500

    init

--- shared/buttons/button-with-icon.vue
+++ shared/buttons/button-with-icon.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-      hello header
+
   </div>
 </template>
 
---
+++ shared/buttons/index.js
@@ -0,0 +1,7 @@
+import button from './button.vue';
+import ButtonWithIcon from './button-with-icon.vue';
+
+export {
+    button,
+    ButtonWithIcon
+}
