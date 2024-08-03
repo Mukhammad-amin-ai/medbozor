d30870abde6cd7039cda87706f1a3811b493afac
Akhmadov Muhammadamin <116802751+Mukhammad-amin-ai@users.noreply.github.com>
2024-08-03 07:49:47 +0500

    [u] added product items at slider in product-description

--- src/widgets/product-description/components/trends.vue
+++ src/widgets/product-description/components/trends.vue
@@ -1,11 +1,118 @@
 <template>
-  <div>
-
+  <div class="mt-10">
+    <div class="text-2xl font-medium">
+      Ommabop mahsulotlar
+    </div>
+    <div class="mt-10 ">
+      <Swiper :navigation="true" :slides-per-view='4' :modules="option" class="mySwiper h-[50vh]">
+        <SwiperSlide v-for="(item,index) in products">
+          <ProductItem
+              :key="index"
+              :title="item.title"
+              :img="item.img"
+              :price="item.price"
+              :discount="item.discount"
+              :has-discount="item.hasDiscount"
+          />
+        </SwiperSlide>
+      </Swiper>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { ProductItem } from "@/shared/product";
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Navigation } from 'swiper/modules';
+import { productItem } from "@/shared/utils/images/index.js";
 
+const products = ref( [
+  {
+    title: 'Tibbiy forma',
+    img: productItem,
+    price: '226 000',
+    discount: '256 000',
+    hasDiscount: false
+  },
+  {
+    title: 'Tibbiy forma',
+    img: productItem,
+    price: '226 000',
+    discount: '256 000',
+    hasDiscount: true
+  },
+  {
+    title: 'Tibbiy forma',
+    img: productItem,
+    price: '226 000',
+    discount: '256 000',
+    hasDiscount: false
+  },
+  {
+    title: 'Tibbiy forma',
+    img: productItem,
+    price: '226 000',
+    discount: '256 000',
+    hasDiscount: false
+  },
+  {
+    title: 'Tibbiy forma',
+    img: productItem,
+    price: '226 000',
+    discount: '256 000',
+    hasDiscount: false
+  },
+  {
+    title: 'Tibbiy forma',
+    img: productItem,
+    price: '226 000',
+    discount: '256 000',
+    hasDiscount: true
+  },
+  {
+    title: 'Tibbiy forma',
+    img: productItem,
+    price: '226 000',
+    discount: '256 000',
+    hasDiscount: false
+  },
+  {
+    title: 'Tibbiy forma',
+    img: productItem,
+    price: '226 000',
+    discount: '256 000',
+    hasDiscount: false
+  },
+  {
+    title: 'Tibbiy forma',
+    img: productItem,
+    price: '226 000',
+    discount: '256 000',
+    hasDiscount: false
+  },
+  {
+    title: 'Tibbiy forma',
+    img: productItem,
+    price: '226 000',
+    discount: '256 000',
+    hasDiscount: true
+  },
+  {
+    title: 'Tibbiy forma',
+    img: productItem,
+    price: '226 000',
+    discount: '256 000',
+    hasDiscount: false
+  },
+  {
+    title: 'Tibbiy forma',
+    img: productItem,
+    price: '226 000',
+    discount: '256 000',
+    hasDiscount: false
+  }
+] )
+let option = ref( [ Navigation ] )
 </script>
 
 <style scoped>
